2aa9989ea774e00e5a5e25faa3c759a04f13e794
pcatel <pcatel@pascalcatel.com>
2023-08-03 11:41:47 +0200

    maj barre de navigation

--- lib/widgets/button_navigation.dart
+++ lib/widgets/button_navigation.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'ecran1.dart';
+import 'ecran2.dart';
+import 'ecran3.dart';
+import 'ecran4.dart';
+import 'ecran5.dart';
+import 'ecran6.dart';
+
+
+ // Importez l'écran que vous souhaitez ouvrir
 
 class BarreIcones extends StatefulWidget {
   const BarreIcones({Key? key}) : super(key: key);
@@ -10,15 +19,14 @@ class BarreIcones extends StatefulWidget {
 class _BarreIconesState extends State<BarreIcones> {
   int _selectedIndex = 0;
 
-  // Liste des icônes pour la navigation
-  static const List<IconData> _icons = [
-    Icons.category,
-    Icons.search,
-    Icons.add,
-    Icons.favorite,
-    Icons.search,
-    Icons.book,
-  
+  // Liste des images pour la navigation
+  static const List<String> _images = [
+    'assets/images/livres.jpg',
+    'assets/images/genre.jpg',
+    'assets/images/auteur.jpg',
+    'assets/images/localisation.jpg',
+    'assets/images/rechercher.jpg',
+    'assets/images/aPropos.jpg',
   ];
 
   // Méthode pour gérer le changement d'index de l'icône sélectionnée
@@ -26,6 +34,49 @@ class _BarreIconesState extends State<BarreIcones> {
     setState(() {
       _selectedIndex = index;
     });
+
+    // Effectuez la navigation vers l'écran correspondant ici
+    switch (index) {
+      case 0:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Ecran1()),
+        );
+        break;
+      case 1:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Ecran2()),
+        );
+        break;
+        case 2:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Ecran3()),
+        );
+        break;
+        case 3:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Ecran4()),
+        );
+        break;
+        case 4:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Ecran5()),
+        );
+        break;
+        case 5:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Ecran6()),
+        );
+        break;
+      
+      default:
+        break;
+    }
   }
 
   @override
@@ -33,11 +84,15 @@ class _BarreIconesState extends State<BarreIcones> {
     return BottomNavigationBar(
       currentIndex: _selectedIndex,
       onTap: _onItemTapped,
-      type: BottomNavigationBarType.fixed, // Pour afficher toutes les icônes, même avec plus de 3 éléments
-      items: _icons.map((IconData icon) {
+      type: BottomNavigationBarType.fixed,
+      items: _images.map((String imagePath) {
         return BottomNavigationBarItem(
-          icon: Icon(icon),
-          label: '', // Vous pouvez laisser une chaîne vide si vous ne souhaitez pas afficher d'étiquette sous l'icône
+          icon: Image.asset(
+            imagePath,
+            height: 24,
+            width: 24,
+          ),
+          label: '',
         );
       }).toList(),
     );
