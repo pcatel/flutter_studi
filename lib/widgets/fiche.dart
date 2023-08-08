b7e2fb685ac92efdcf5288072b345928208873bb
pcatel <pcatel@pascalcatel.com>
2023-08-08 15:55:50 +0200

    test

--- lib/widgets/fiche.dart
+++ lib/widgets/fiche.dart
@@ -6,6 +6,8 @@ class FichePage extends StatelessWidget {
 
   const FichePage({required this.livre, Key? key}) : super(key: key);
 
+  final double imageSize = 100; // Taille des images
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,52 +17,107 @@ class FichePage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            ListTile(
-              title: Text('Titre'),
-              subtitle: Text(livre.titre),
+            SizedBox(height: 16),
+            // Auteur
+            _buildRowWithColor(
+              child: Row(
+                children: [
+                  Text('Auteur:'),
+                  SizedBox(width: 8),
+                  Text('${livre.prenomAuteur} ${livre.nomAuteur}'),
+                ],
+              ),
+              color: Colors.blue,
             ),
-            ListTile(
-              title: Text('Date de création'),
-              subtitle: Text(livre.dateCreation),
+            SizedBox(height: 16),
+            // Photo et Résumé
+            _buildRowWithColor(
+              child: Flex(
+                direction: Axis.horizontal,
+                children: [
+                  Flexible(child: _buildImageContainer(livre.photo)),
+                  SizedBox(width: 16),
+                  Flexible(child: _buildImageContainer(livre.resume.url)),
+                ],
+              ),
+              color: Colors.green,
             ),
-            ListTile(
-              title: Text('Auteur'),
-              subtitle: Text('${livre.prenomAuteur} ${livre.nomAuteur}'),
+            SizedBox(height: 16),
+            // Genre
+            _buildRowWithColor(
+              child: Row(
+                children: [
+                  Text('Genre:'),
+                  SizedBox(width: 8),
+                  Text(livre.genre),
+                ],
+              ),
+              color: Colors.orange,
             ),
-            ListTile(
-              title: Text('Résumé'),
-              subtitle: livre.resume.url.isNotEmpty
-                  ? Image.network(Uri.parse("https://www.pascalcatel.com/biblio/${_sanitizeString(livre.resume.url)}").toString())
-                  : Text('Pas de résumé avec image'),
+            SizedBox(height: 16),
+            // Localisation et Rayon
+            _buildRowWithColor(
+              child: Row(
+                children: [
+                  Text('Localisation:'),
+                  SizedBox(width: 8),
+                  Text(livre.localisation),
+                  SizedBox(width: 16),
+                  Text('Rayon:'),
+                  SizedBox(width: 8),
+                  Text(livre.rayon),
+                ],
+              ),
+              color: Colors.purple,
             ),
-            ListTile(
-              title: Text('Genre'),
-              subtitle: Text(livre.genre),
+            SizedBox(height: 16),
+            // Commentaire
+            _buildRowWithColor(
+              child: Row(
+                children: [
+                  Text('Commentaire:'),
+                  SizedBox(width: 8),
+                  Text(livre.commentaire),
+                ],
+              ),
+              color: Colors.red,
             ),
-            ListTile(
-              title: Text('Localisation'),
-              subtitle: Text(livre.localisation),
+            SizedBox(height: 16),
+            // Pret
+            _buildRowWithColor(
+              child: Row(
+                children: [
+                  Text('Pret:'),
+                  SizedBox(width: 8),
+                  Text(livre.pret),
+                ],
+              ),
+              color: Colors.yellow,
             ),
-            ListTile(
-              title: Text('Rayon'),
-              subtitle: Text(livre.rayon),
-            ),
-            ListTile(
-              title: Text('Commentaire'),
-              subtitle: Text(livre.commentaire),
-            ),
-            ListTile(
-              title: Text('Pret'),
-              subtitle: Text(livre.pret),
-            ),
-            ListTile(
-              title: Text('Photo'),
-              subtitle: livre.photo.isNotEmpty
-                  ? Image.network(Uri.parse("https://www.pascalcatel.com/biblio/${_sanitizeString(livre.photo)}").toString())
-                  : Text('Pas de photo disponible'),
-            ),
+            SizedBox(height: 16),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildRowWithColor({required Widget child, required Color color}) {
+    return Container(
+      color: color,
+      padding: EdgeInsets.all(8),
+      child: child,
+    );
+  }
+
+  Widget _buildImageContainer(String url) {
+    return Container(
+      width: imageSize,
+      height: imageSize,
+      child: Align(
+        alignment: Alignment.center,
+        child: url.isNotEmpty
+            ? Image.network(Uri.parse("https://www.pascalcatel.com/biblio/${_sanitizeString(url)}").toString())
+            : Placeholder(), // Utilisation d'un Placeholder en cas d'absence d'image
       ),
     );
   }
